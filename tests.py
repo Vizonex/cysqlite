77d31909e8205734957b4de2b62fa3b7bb16e61b
Charles Leifer <coleifer@gmail.com>
2019-05-19 22:12:36 -0500

    Couple more tests.

--- tests.py
+++ tests.py
@@ -21,3 +21,14 @@ for row in curs:
 
 conn.execute_simple('drop table kv')
 conn.close()
+
+conn.connect()
+conn.execute('create table foo (id integer not null primary key, key text)')
+conn.execute('insert into foo (key) values (?), (?), (?), (?)',
+             ('k1', 'k2', 'k3', 'k4'))
+for row in conn.execute('select * from foo'):
+    print(row[0], '->', row[1])
+
+conn.execute('delete from foo where id < ?', (3,))
+print(conn.changes())
+conn.close()
